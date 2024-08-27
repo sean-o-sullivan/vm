67dc16b97c3be11a5ca9d11034775c1bafd5ccc9
sean-o-sullivan <seangerardosullivan@gmail.com>
2024-08-27 19:46:58 +0000

    ollama test

---
+++ embeddingGen/ollamaTest.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "http://127.0.0.1:11434/v1/chat/"
+
+payload = {
+	"model": "llama-3.1-8b",
+	"prompt": "Hello, how are you?"
+}
+
+headers = {
+	"Content-Type: "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+
+print(f"status code: {response.status_code}")
+
+print(f"response: {response.text}")
+
