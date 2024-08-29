003bd5bd69f95a0e63c9689de41ee34a6cc61ae6
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-08-29 20:06:29 +0100

    Update samples.py

--- embeddingGen/Thursday/samples.py
+++ embeddingGen/Thursday/samples.py
@@ -3,10 +3,10 @@ import csv
 import random
 import stanza
 import logging
+import json
 from multiprocessing import Pool
 from tqdm import tqdm
 from collections import defaultdict
-
 
 SAMPLES_PER_AUTHOR = 100
 SAMPLE_LENGTH = 1300  # Number of characters per sample
@@ -72,23 +72,48 @@ def process_book(args):
     logging.info(f"Finished processing {file_path}.")
     return len(samples)
 
+def parse_custom_id(custom_id):
+    # Extract author and file parts from the custom_id
+    author_part, file_part = custom_id.split('-', 1)
+    author_name = author_part.replace('_', '__')
+    file_name = file_part.split('_', 1)[-1]  # Extract filename part
+    return author_name, file_name
+
+def load_eligible_books(jsonl_path):
+    eligible_books = defaultdict(set)
+    with open(jsonl_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            entry = json.loads(line)
+            custom_id = entry['custom_id']
+            response_content = entry['response']['body']['choices'][0]['message']['content']
+            if response_content == "YES":
+                author_name, file_name = parse_custom_id(custom_id)
+                eligible_books[author_name].add(file_name)
+    return eligible_books
+
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     input_dir = '/home/aiadmin/Desktop/datasets/bigText'
     sample_file = 'samples.csv'
+    jsonl_path = 'embeddingGen/batch_dataset_classification_output.jsonl'
+
+    # Load the set of eligible books
+    eligible_books = load_eligible_books(jsonl_path)
 
     # Create a list to store all eligible book paths
     all_books = []
 
-    # Collect all book paths
+    # Collect all eligible book paths
     for author_dir in os.listdir(input_dir):
         author_path = os.path.join(input_dir, author_dir)
         if os.path.isdir(author_path):
-            for book_file in os.listdir(author_path):
-                book_path = os.path.join(author_path, book_file)
-                if os.path.isfile(book_path) and os.path.getsize(book_path) >= MIN_BOOK_LENGTH + NO_TOUCH_ZONE:
-                    all_books.append((book_path, author_dir))
+            if author_dir in eligible_books:
+                for book_file in os.listdir(author_path):
+                    book_path = os.path.join(author_path, book_file)
+                    if os.path.isfile(book_path) and os.path.getsize(book_path) >= MIN_BOOK_LENGTH + NO_TOUCH_ZONE:
+                        if book_file in eligible_books[author_dir]:
+                            all_books.append((book_path, author_dir))
 
     # Shuffle and limit to MAX_BOOKS
     random.shuffle(all_books)
