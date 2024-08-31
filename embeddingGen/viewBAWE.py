bd7c7f945f9312c8b1a7c6f70d690323b9ad44d4
sean-o-sullivan <seangerardosullivan@gmail.com>
2024-08-31 15:50:27 +0000

    BAWE initialised

--- embeddingGen/viewBAWE.py
+++ embeddingGen/viewBAWE.py
@@ -1,5 +1,6 @@
 import csv
 csv_file_path = 'authors_texts.csv'  
+csv_file_path = 'BAWE_texts.csv'  
 
 with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file)
@@ -9,7 +10,8 @@ with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
         author_id = row[0]
         text = row[1]
         print(f"Author ID: {author_id}")
-        print(f"Text snippet: {text[:100]}...")  
+        print(f"Text snippet: {text}...") 
         print("-" * 50)
+        input(">>> press enter to continue")
 
 print("Test is finally completed. See output to ensure the text is properly formatted.")
