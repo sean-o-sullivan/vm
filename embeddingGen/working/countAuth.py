62f9765488576e82ed4f9050131029fbe838b45e
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-09-01 05:24:17 +0100

    Create countAuth.py

---
+++ embeddingGen/working/countAuth.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def count_unique_authors(csv_file):
+    df = pd.read_csv(csv_file)
+    unique_authors = df['author'].nunique()
+    return unique_authors
+
+if __name__ == "__main__":
+    csv_file = 'ABB_30.csv'
+    unique_authors_count = count_unique_authors(csv_file)
+    print(f"The number of unique authors in {csv_file} is: {unique_authors_count}")
