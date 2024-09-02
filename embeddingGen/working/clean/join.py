c65408bdd5c070ce91ac1782d1d7cb4590be35dd
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-09-02 21:17:05 +0100

    Update join.py

--- embeddingGen/working/clean/join.py
+++ embeddingGen/working/clean/join.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 csv_a = pd.read_csv('path_to_csv_a.csv')
-filtered_csv_b = pd.read_csv('filtered_csv_b.csv')
+csv_b = pd.read_csv('path_to_csv_b.csv')
 
 csv_a['original_text'] = csv_a['original_text'].astype(str).str[:100]
-filtered_csv_b['original_text'] = filtered_csv_b['original_text'].astype(str).str[:100]
+csv_b['original_text'] = csv_b['original_text'].astype(str).str[:100]
+
+filtered_csv_b = csv_b[csv_b['original_text'].isin(csv_a['original_text'])]
 
 merged_df = pd.merge(csv_a, filtered_csv_b[['original_text', 'embedding']], on='original_text', how='left')
 merged_df.to_csv('csv_a_with_embeddings.csv', index=False)
