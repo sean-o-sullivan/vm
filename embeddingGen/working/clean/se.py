2eea101aafd363d2e93b27b942760fed5dcabc86
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-09-02 20:03:04 +0100

    Update se.py

--- embeddingGen/working/clean/se.py
+++ embeddingGen/working/clean/se.py
@@ -1,14 +1,45 @@
 import pandas as pd
+import ast
+import random
 
-text_file_path = '/home/aiadmin/Desktop/code/vm/embeddingGen/working/ABB_30.csv'
-embeddings_file_path = '/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalisedandready/BB_30.csv'
-output_file_path = '/home/aiadmin/Desktop/code/vm/embeddingGen/working/clean/combined_texts_and_embeddings.csv'
-text_df = pd.read_csv(text_file_path)
-embeddings_df = pd.read_csv(embeddings_file_path)
+def load_csv(file):
+    return pd.read_csv(file)
 
-embeddings_df = embeddings_df.drop(columns=['EmbeddingID'])
-merged_df = pd.merge(text_df, embeddings_df, left_on='author', right_on='authorID')
-merged_df['embedding'] = merged_df.iloc[:, 3:].values.tolist()
-final_df = merged_df[['author', 'original_text', 'embedding']]
+def string_to_list(s):
+    try:
+        return ast.literal_eval(s)
+    except:
+        return s
 
-final_df.to_csv(output_file_path, index=False)
+def combine_csvs(mimics, topics, output):
+
+    mimic_dfs = [load_csv(file) for file in mimics]
+    topic_dfs = [load_csv(file) for file in topics]
+    df = mimic_dfs[0][['author']].copy()
+    
+    author_text_map = {}
+    for author in df['author'].unique():
+        source_df = random.choice(mimic_dfs + topic_dfs)
+        author_text_map[author] = source_df[source_df['author'] == author]['original_text'].sample(n=1, random_state=42).values[0]
+
+    df['original_text'] = df['author'].map(author_text_map)
+    for i, file in enumerate(mimics):
+        model_name = file.split('_')[3]  # Extracting GPT model name from the filename
+        column_name = f'mimic_{model_name}_embedding'
+        df[column_name] = mimic_dfs[i]['embedding'].apply(string_to_list)
+    
+    for i, file in enumerate(topics):
+        model_name = file.split('_')[3]  
+        column_name = f'topic_{model_name}_embedding'
+        df[column_name] = topic_dfs[i]['embedding'].apply(string_to_list)
+    
+    df.to_csv(output, index=False)
+
+if __name__ == "__main__":
+    combine_csvs(['/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_mimicry_samples_GPT3ABB_30_embeddings.csv',
+                  '/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_mimicry_samples_GPT4TABB_30_embeddings.csv',
+                  '/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_mimicry_samples_GPT4oABB_30_embeddings.csv'],
+                 ['/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_topic_based_samples_GPT3ABB_30_embeddings.csv',
+                  '/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_topic_based_samples_GPT4TABB_30_embeddings.csv',
+                  '/home/aiadmin/Desktop/code/vm/embeddingGen/working/embeddings/normalized_adversarial_csvs/normalized_topic_based_samples_GPT4oABB_30_embeddings.csv'],
+                 'combined_embeddings.csv')
