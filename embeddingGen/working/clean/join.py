d752d63982009c8b7a15bad55d0aab4c632920c3
sean-o-sullivan <seangerardosullivan@gmail.com>
2024-09-02 20:35:57 +0000

    samples

--- embeddingGen/working/clean/join.py
+++ embeddingGen/working/clean/join.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-csv_a = pd.read_csv('path_to_csv_a.csv')
-csv_b = pd.read_csv('path_to_csv_b.csv')
+csv_a = pd.read_csv('AGG_combined_embeddings.csv') #gpt data
+csv_b = pd.read_csv('AGG_combined_texts_and_embeddings.csv') # only 3 columns
+
 csv_a['original_text'] = csv_a['original_text'].astype(str).str[:100]
 csv_b['original_text'] = csv_b['original_text'].astype(str).str[:100]
 filtered_csv_b = csv_b[csv_b['original_text'].isin(csv_a['original_text'])]
-merged_df = pd.merge(csv_a, filtered_csv_b[['original_text', 'embedding']], on='original_text', how='left')
+merged_df = pd.merge(csv_a, filtered_csv_b[['original_text', 'embeddings']], on='original_text', how='left')
 merged_df.to_csv('csv_a_with_embeddings.csv', index=False)
 
 print("CSV A with embeddings has been saved as 'csv_a_with_embeddings.csv'.")
